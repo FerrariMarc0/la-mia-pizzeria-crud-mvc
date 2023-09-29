b85cb4e72329f8189b5a578e13211b2c31c04823
FerrariMarc0 <marco1010@live.it>
2023-09-29 12:44:00 +0200

    Aggiungere i file di progetto.

---
+++ la-mia-pizzeria-static/Models/Pizza.cs
@@ -0,0 +1,19 @@
+﻿namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string UrlImage { get; set; }
+        public decimal Price { get; set; }
+
+        public Pizza(string name, string description, string image ,decimal price)
+        {
+            Name = name;
+            Description = description;
+            UrlImage = image;
+            Price = price;
+        }
+    }
+}
